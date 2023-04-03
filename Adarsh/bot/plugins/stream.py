e497d06a4cf164b9e34f9ce6360822ebdb3d06ca
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-04-04 01:27:48 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -150,6 +150,7 @@ async def channel_receive_handler(bot, broadcast):
         )
         await bot.edit_message_reply_markup(
             chat_id=broadcast.chat.id,
+            message_id=broadcast.id,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("⚡ ᴡᴀᴛᴄʜ ⚡", url=stream_link),
